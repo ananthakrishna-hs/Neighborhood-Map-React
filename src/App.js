27b105e02ed889888656dc4fbbac21e8dce497fa
Ananthakrishna H S <h.s.ananthakrishna@gmail.com>
2018-11-16 07:09:25 +0530

    bugfix: Removed infinite firing of requests on error.
    This would potentially could cause the browser to crash. Now the main
    part of the application wont render if error and is blocked till reload
    and noerror.

--- src/App.js
+++ src/App.js
@@ -118,131 +118,145 @@ class App extends Component {
     
     return (
       <div>
-        {this.state.modalDetail.venue && 
-        (
-          <Modal 
-          show={this.state.modalShown}
-          onHide={this.closeModal}
-          autoFocus={true}
-          aria-label="Modal Window"
-          onEnter={event => this.hoverStart(this.state.modalDetail.venue.id)}
-          onExit={event => this.hoverStop()}
-          >
-            <ModalHeader>
-              <ModalTitle 
-              tabIndex="0" 
-              ref={(title) => { this.modalTitle = title; }}
-              onKeyDown={e => this.titleFocus(e)}
+        {this.state.errorComponent && (
+          <header tabIndex="0">
+            <Jumbotron>
+              <h1 tabIndex="0">
+                Sameepa
+              </h1>
+              <p tabIndex="0">
+                One-stop destination to find nearby places.
+              </p>
+                <h3 tabIndex="0" aria-live="assertive">
+                  <Label bsStyle="danger">
+                    Some or no content could be loaded.<br /> Please try again later
+                  </Label>
+                </h3>
+            </Jumbotron>
+          </header>
+        )}
+        {!this.state.errorComponent && (
+          <div>
+            {this.state.modalDetail.venue && 
+            (
+              <Modal 
+              show={this.state.modalShown}
+              onHide={this.closeModal}
+              autoFocus={true}
+              aria-label="Modal Window"
+              onEnter={event => this.hoverStart(this.state.modalDetail.venue.id)}
+              onExit={event => this.hoverStop()}
               >
-                {this.state.modalDetail.venue.name}
-              </ModalTitle>
-            </ModalHeader>
-            <ModalBody tabIndex="0" aria-label="Venue Details">
-              <div tabIndex="0">Categories:</div>
-              {this.state.modalDetail.venue.categories.length && (
-                <div>
-                  <Panel tabIndex="0">
-                    {this.state.modalDetail.venue.categories.map(category => category.name)}
-                  </Panel>
+                <ModalHeader>
+                  <ModalTitle 
+                  tabIndex="0" 
+                  ref={(title) => { this.modalTitle = title; }}
+                  onKeyDown={e => this.titleFocus(e)}
+                  >
+                    {this.state.modalDetail.venue.name}
+                  </ModalTitle>
+                </ModalHeader>
+                <ModalBody tabIndex="0" aria-label="Venue Details">
+                  <div tabIndex="0">Categories:</div>
+                  {this.state.modalDetail.venue.categories.length && (
+                    <div>
+                      <Panel tabIndex="0">
+                        {this.state.modalDetail.venue.categories.map(category => category.name)}
+                      </Panel>
+                      <br />
+                    </div>
+                  )}
                   <br />
-                </div>
-              )}
-              <br />
-              <Label bsStyle="default" className="modal-label-left" tabIndex="0">Location: {this.state.modalDetail.venue.location.country}</Label>
-              <Label bsStyle="info" className="modal-label-right" tabIndex="0">Timezone: {this.state.modalDetail.venue.timeZone}</Label>
-            </ModalBody>
-            <ModalFooter>
-              <div tabIndex="0" aria-label="Data attribution">
-                <p tabIndex="0">Data by FOURSQUARE API</p>
-                <Button 
-                bsStyle="success" 
-                href={this.state.modalDetail.venue.shortUrl} 
-                target="_blank"
-                className="modal-label-left"
-                >
-                  FOURSQUARE link of the Venue
-                </Button>
-              </div>
-              <Button 
-              bsStyle="danger" 
-              onClick={this.closeModal} 
-              ref={(close) => { this.closeButton = close; }}
-              tabIndex="0"
-              onKeyDown={e => this.closeFocus(e)}
-              className="modal-label-right"
-              aria-label="Modal Close"
-              >
-                Close
-              </Button>
-              
-            </ModalFooter>
-          </Modal>
-        )
-        }
-        <header tabIndex="0">
-          <Jumbotron>
-            <h1 tabIndex="0">
-              Sameepa
-            </h1>
-            <p tabIndex="0">
-              One-stop destination to find nearby places.
-            </p>
-            {this.state.errorComponent && (
-              <h3 tabIndex="0" aria-live="assertive">
-                <Label bsStyle="danger">
-                  Some or no content could be loaded.<br /> Please try again later
-                </Label>
-              </h3>
-            )}
-          </Jumbotron>
-        </header>
-        <main>
-          <Grid>
-            <Row className="main-container">
-              <Col sm={4} className="list-container" tabIndex="0" aria-label="Filter list">
-                <Navbar>
-                  <Navbar.Header>
-                    <Navbar.Toggle />
-                  </Navbar.Header>
-                  <Navbar.Collapse>
-                    <label for="search">Filter Locations:</label>
-                    <input type="text" id="search" className="filter-text" onChange={e => this.querying(e.target.value)} />
-                    <ul className="list-items">
-                      {
-                        queriedVenues.map(element => (
-                          <li 
-                          key={element.id} 
-                          className="list-item"
-                          onClick={event => this.toggleModal(element.id)}
-                          onMouseOver={event => this.hoverStart(element.id)}
-                          onMouseOut={event => this.hoverStop()}
-                          tabIndex="0"
-                          role="button"
-                          >
-                            {element.name}
-                          </li>
-                        ))
-                      }
-                    </ul>
-                  </Navbar.Collapse>
-                </Navbar>
-              </Col>
-              <Col sm={8} className="map-container" tabIndex="0" aria-label="Map application">
-                <MyMapComponent
-                markers_loc={queriedVenues}
-                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDT23otK1Zca1ko0AT1Yu1ntojCMHTVzfY"
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `400px` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-                center={this.state.center}
-                triggerUpdate={this.updateMarkers}
-                triggerClick={this.toggleModal}
-                hover={this.state.hover}
-                />
-              </Col>
-            </Row>
-          </Grid>
-        </main>
+                  <Label bsStyle="default" className="modal-label-left" tabIndex="0">Location: {this.state.modalDetail.venue.location.country}</Label>
+                  <Label bsStyle="info" className="modal-label-right" tabIndex="0">Timezone: {this.state.modalDetail.venue.timeZone}</Label>
+                </ModalBody>
+                <ModalFooter>
+                  <div tabIndex="0" aria-label="Data attribution">
+                    <p tabIndex="0">Data by FOURSQUARE API</p>
+                    <Button 
+                    bsStyle="success" 
+                    href={this.state.modalDetail.venue.shortUrl} 
+                    target="_blank"
+                    className="modal-label-left"
+                    >
+                      FOURSQUARE link of the Venue
+                    </Button>
+                  </div>
+                  <Button 
+                  bsStyle="danger" 
+                  onClick={this.closeModal} 
+                  ref={(close) => { this.closeButton = close; }}
+                  tabIndex="0"
+                  onKeyDown={e => this.closeFocus(e)}
+                  className="modal-label-right"
+                  aria-label="Modal Close"
+                  >
+                    Close
+                  </Button>
+                  
+                </ModalFooter>
+              </Modal>
+            )
+            }
+            <header tabIndex="0">
+              <Jumbotron>
+                <h1 tabIndex="0">
+                  Sameepa
+                </h1>
+                <p tabIndex="0">
+                  One-stop destination to find nearby places.
+                </p>
+              </Jumbotron>
+            </header>
+            <main>
+              <Grid>
+                <Row className="main-container">
+                  <Col sm={4} className="list-container" tabIndex="0" aria-label="Filter list">
+                    <Navbar>
+                      <Navbar.Header>
+                        <Navbar.Toggle />
+                      </Navbar.Header>
+                      <Navbar.Collapse>
+                        <label htmlFor="search">Filter Locations:</label>
+                        <input type="text" id="search" className="filter-text" onChange={e => this.querying(e.target.value)} />
+                        <ul className="list-items">
+                          {
+                            queriedVenues.map(element => (
+                              <li 
+                              key={element.id} 
+                              className="list-item"
+                              onClick={event => this.toggleModal(element.id)}
+                              onMouseOver={event => this.hoverStart(element.id)}
+                              onMouseOut={event => this.hoverStop()}
+                              tabIndex="0"
+                              role="button"
+                              >
+                                {element.name}
+                              </li>
+                            ))
+                          }
+                        </ul>
+                      </Navbar.Collapse>
+                    </Navbar>
+                  </Col>
+                  <Col sm={8} className="map-container" tabIndex="0" aria-label="Map application">
+                    <MyMapComponent
+                    markers_loc={queriedVenues}
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDT23otK1Zca1ko0AT1Yu1ntojCMHTVzfY"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `400px` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                    center={this.state.center}
+                    triggerUpdate={this.updateMarkers}
+                    triggerClick={this.toggleModal}
+                    hover={this.state.hover}
+                    />
+                  </Col>
+                </Row>
+              </Grid>
+            </main>
+          </div>
+        )}
       </div>
     )
   }
